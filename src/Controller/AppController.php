ba2ebb293d5add59f60b6af72ac60a7a236f0cc6
Fabricio Jakubec <fabricio872@gmail.com>
2023-02-12 19:43:22 +0100

    created github section added github api integration added bootstrap icons

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,24 +6,36 @@ namespace App\Controller;
 
 use App\Entity\PrintedProject;
 use App\Entity\WebProject;
+use App\Service\GithubReader;
 use Carbon\Carbon;
+use DateInterval;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 #[Route('/', name: 'app.')]
 class AppController extends AbstractController
 {
+    public function __construct(
+        private readonly GithubReader $githubReader,
+        private readonly CacheInterface $cache,
+        private readonly ParameterBagInterface $parameterBag
+    ) {
+    }
+
     #[Route('/', name: 'index')]
     public function index(Request $request, EntityManagerInterface $em): Response
     {
         Carbon::setLocale($request->getLocale());
-        $startDate = Carbon::create(new DateTime('1.12.2017 08:00'));
+        $startDate = Carbon::create(new DateTime($this->parameterBag->get('symfonyStartDate')));
         if (! $startDate) {
             throw new Exception("Wrong Start date provided");
         }
@@ -31,7 +43,8 @@ class AppController extends AbstractController
         return $this->render('app/index.html.twig', [
             'symfonyInterval' => $startDate->longRelativeToNowDiffForHumans(parts: 5),
             'webProjects' => $em->getRepository(WebProject::class)->findAll(),
-            'printedProjects' => $em->getRepository(PrintedProject::class)->findAll()
+            'printedProjects' => $em->getRepository(PrintedProject::class)->findAll(),
+            'githubProjects' => $this->getGithubItems()
         ]);
     }
 
@@ -40,5 +53,24 @@ class AppController extends AbstractController
     {
         $this->addFlash('spiderman-easteregg', true);
         return $this->redirectToRoute('app.index');
+    }
+
+    private function getGithubItems(): array
+    {
+        return $this->cache->get('github_items', function (ItemInterface $item) {
+            $item->expiresAfter(DateInterval::createFromDateString('1 month'));
+            $items = [];
+            foreach ($this->githubReader->listRepositories() as $repository) {
+                if ($repository['description']) {
+                    $items[] = [
+                        'url' => $repository['name'],
+                        'title' => $repository['name'],
+                        'description' => $repository['description'],
+                        'stars' => $repository['stargazers_count']
+                    ];
+                }
+            }
+            return $items;
+        });
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.woff2?$/,
+            type: "asset/resource",
+        }]
+    }
+}
+
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
